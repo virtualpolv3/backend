18528f6243b03394d71995debb16d2eb002cdbf7
David del Amo Mateos <david.delamo@solera.com>
2020-02-14 12:41:33 +0100

    Initial release

---
+++ src/main/java/com/dama/virtualpol/VirtualPolApplication.java
@@ -0,0 +1,13 @@
+package com.dama.virtualpol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualPolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualPolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dama/virtualpol/VirtualPolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dama.virtualpol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualPolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
